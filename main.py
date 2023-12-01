0400bde31213d3f373f9cc068110cf5c2c926a10
PernatiyZybr <orlov.vv@phystech.edu>
2023-12-01 16:21:11 +0300

    add points to images, small rework motion control

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import numpy as np
-import PIL
 from PIL import Image, ImageDraw
 from tiny_functions import *
 SCREEN_SIZE = (1920, 1080)
 NEWSIE = (350, 350)
+RADIUS = 10
 
 class Object:
-    def __init__(self, im: Image, center: list, x_m: float = 0, y_m: float = 0, vx: float = 0, vy: float = 0):
+    def __init__(self, im: Image, center: list, front_and_back: list, color_point: str = 'white',
+                 x_m: float = 0, y_m: float = 0, vx: float = 0, vy: float = 0):
         self.im = im
         self.center = center
+        self.front_and_back = front_and_back
+        self.color_point = color_point
 
         self.x_m = x_m
         self.y_m = y_m
         self.vx_kmh = vx
         self.vy_kmh = vy
+        self.integral = 0.
 
-    def meter2pixel(self, h: int, x_vec, y_vec, y_past: float = 0):
-        tmp = self.x_m * h * x_vec + (self.y_m * h + y_past) * y_vec
+    def meter2pixel(self, h: int, x_vec, y_vec, y_past: float = 0, point: str = 'center'):
+        x_m, y_m = (self.x_m, self.y_m)
+        if point == 'front':
+            y_m += self.front_and_back[0]
+        if point == 'back':
+            y_m += self.front_and_back[1]
+        tmp = x_m * h * x_vec + (y_m * h + y_past) * y_vec
         return int(round(tmp[0])), int(round(tmp[1]))
 
     def kmh2pxs(self, h: int):
@@ -28,12 +37,17 @@ class Object:
         self.x_m += self.vx_kmh / 3.6 * dt
         self.y_m += self.vy_kmh / 3.6 * dt
 
-    def add_to_draw(self, im: Image, x_vec, y_vec, y_past: float, d: int):
+    def add_to_draw(self, im: Image, draw: ImageDraw, x_vec, y_vec, y_past: float, d: int):
         w, h = SCREEN_SIZE
         x, y = self.meter2pixel(d, x_vec, y_vec, y_past)
         x_screen = int(w//2) - self.center[0]
         y_screen = int(h//2) - self.center[1]
-        im.paste(self.im, (-x + x_screen, -y + y_screen), self.im)
+        im.paste(self.im, (x_screen - x, y_screen - y), self.im)
+        for xy in [self.meter2pixel(d, x_vec, y_vec, y_past, point='front'),
+                   self.meter2pixel(d, x_vec, y_vec, y_past, point='back'), (x, y)]:
+            draw.ellipse((int(w//2) - xy[0] - RADIUS, int(h//2) - xy[1] - RADIUS,
+                         int(w//2) - xy[0] + RADIUS, int(h//2) - xy[1] + RADIUS),
+                         fill=self.color_point, outline=(0, 0, 0))
 
 class FrontCar(Object):
     def new_process(self, dt: float, t: float, v0):
@@ -43,17 +57,19 @@ class FrontCar(Object):
         self.y_m += self.vy_kmh / 3.6 * dt
 
 class MyCar(Object):
-    def get_acceleration(self, obj: any, a_max: float = 1e1):
-        y_m_1, vy_kmh_1 = (obj.y_m, obj.vy_kmh)
-        a = - 0.005 * (self.y_m - y_m_1 + 4.5) - 0.1 * (self.vy_kmh - vy_kmh_1)  #  + 0.5 * abs(vy_kmh_1)
+    def get_acceleration(self, obj: any, a_max: float = 1e10, dt: float = 0.5):
+        x_diff = ((self.y_m + self.front_and_back[0]) - (obj.y_m + obj.front_and_back[1]) + 1.5)
+        v_diff = (self.vy_kmh - obj.vy_kmh)  # + 0.5 * abs(obj.vy_kmh)
+        self.integral += x_diff * dt
+        a = - 0.01 * x_diff - 0.05 * v_diff  # - 0.01 * self.integral
+        a -= 0.15 / x_diff**2
         return [0, clip(a, -a_max, a_max)]
 
 class Display:
     def __init__(self, len_road_meter: int):
         # Изображения
         self.images = {'background': Image.open("img/back.png"),
-                       'cars': [Image.open("img/car1.png").resize(NEWSIE),
-                                Image.open("img/car2.png").resize(NEWSIE).transpose(PIL.Image.FLIP_LEFT_RIGHT),
+                       'cars': [Image.open("img/car1.png").resize(NEWSIE), Image.open("img/car2.png").resize(NEWSIE),
                                 Image.open("img/car_heavy.png"), Image.open("img/car_police.png").resize(NEWSIE)]}
 
         self.h_pixels = 150
@@ -78,9 +94,12 @@ class Process:
         self.d = Display(len_road_meter=int(round(dt * vy * frames)))
 
         # Объекты
-        self.front_car = FrontCar(self.d.images['cars'][0], [int(NEWSIE[0]//2), int(NEWSIE[1]//2) + 20], x_m=0, y_m=2)
-        self.my_car = MyCar(self.d.images['cars'][1], [int(NEWSIE[0]//2), int(NEWSIE[1]//2) + 40], x_m=0, y_m=-1)
-        self.my_car_2 = MyCar(self.d.images['cars'][2], [256, 256 + 50], x_m=0, y_m=-5)
+        self.front_car = FrontCar(self.d.images['cars'][0], [int(NEWSIE[0]//2), int(NEWSIE[1]//2) + 20], x_m=0, y_m=2,
+                                  front_and_back=[-1.1, 1.1], color_point='cyan')
+        self.my_car = MyCar(self.d.images['cars'][1], [int(NEWSIE[0]//2), int(NEWSIE[1]//2) + 40], x_m=0, y_m=-1,
+                            front_and_back=[-1, 1], color_point='magenta')
+        self.my_car_2 = MyCar(self.d.images['cars'][2], [256, 256 + 50], x_m=0, y_m=-5, front_and_back=[-1.8, 1.8],
+                              color_point='navy')
 
     def add_lines(self, img):
         color = "#505050"
@@ -103,9 +122,10 @@ class Process:
         screen = self.d.images['background'].copy()
         draw = ImageDraw.Draw(screen)
         self.add_lines(draw)
-        self.my_car_2.add_to_draw(screen, self.d.directions['x'], self.d.directions['y'], self.y, self.d.h_pixels)
-        self.my_car.add_to_draw(screen, self.d.directions['x'], self.d.directions['y'], self.y, self.d.h_pixels)
-        self.front_car.add_to_draw(screen, self.d.directions['x'], self.d.directions['y'], self.y, self.d.h_pixels)
+        self.my_car_2.add_to_draw(screen, draw, self.d.directions['x'], self.d.directions['y'], self.y, self.d.h_pixels)
+        self.my_car.add_to_draw(screen, draw, self.d.directions['x'], self.d.directions['y'], self.y, self.d.h_pixels)
+        self.front_car.add_to_draw(screen, draw, self.d.directions['x'], self.d.directions['y'], self.y,
+                                   self.d.h_pixels)
         return screen
 
     def process(self):
